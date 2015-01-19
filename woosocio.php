b0859e50b7efa803d442849b1ff98f43b0b7142f
qsheeraz <qsheeraz@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-19 17:41:44 +0000

    new site added http://genialsouls.com/
    git-svn-id: https://plugins.svn.wordpress.org/woosocio/tags/0.6.0@1071188 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- woosocio.php
+++ woosocio.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * Plugin Name: WooSocio
- * Plugin URI: http://shafiamall.co.uk/woosocio/
+ * Plugin URI: http://genialsouls.com/
  * Description: This plugin will upload/post your Woo products to facebook wall as well as facebook pages automatically, when published. Also adds like/share buttons in Woo products.
  * Author: Qamar Sheeraz
- * Author URI: http://shafiamall.co.uk/woosocio/
+ * Author URI: http://genialsouls.com/
  * Version: 0.6.0
  * Stable tag: 0.6.0
  * License: GPL v2 - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
